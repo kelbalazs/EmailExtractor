c90c500a6375eb7a091febcea3cebd1f28dbbebd
Balazs Kelemen <kelbal@gmail.com>
2022-08-14 11:33:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<EmailExtractor />
+</template>
+
+<script>
+import EmailExtractor from './components/EmailExtractor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EmailExtractor
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/EmailExtractor.vue
@@ -0,0 +1,45 @@
+<template>
+ Paste Text: <br>
+    <textarea name="output" cols="50" rows="12" v-model="rawText" placeholder="paste here"></textarea>
+    <br>
+
+<button type="button" @click="extractEmails">Get emails</button><br>
+    <span class="counter"></span><br>
+    Emails in text:<br>
+    <textarea name="output" cols="50" rows="12" v-model="emails"></textarea>
+    <div id="counter">Emails found: {{counter}}</div>
+</template>
+
+<script>  
+export default {
+
+      data(){
+        return {
+            rawText: '',
+            emails:'',
+            counter:0,
+            emailData:[],
+            exp : /([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi,
+     
+            }   
+        },
+
+
+    methods:{
+        
+        extractEmails(){
+            this.emails = this.rawText.match(/([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+            this.emails = [...new Set(this.emails)],
+            this.counter = this.emails.length; 
+            
+        },
+
+        
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
